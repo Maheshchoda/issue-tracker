defb6dc3aeb2bbfb3daf64601f004eaf0586cc04
Mahesh CHODA <choda.maheshbabu@gmail.com>
2024-06-23 17:16:00 +0530

    Build the New Issue page.

--- src/app/issues/page.tsx
+++ src/app/issues/page.tsx
@@ -1,7 +1,8 @@
 import { Button } from '@radix-ui/themes';
+import Link from 'next/link';
 
 const IssuesPage = () => {
-  return <Button>Submit</Button>;
+  return <Button><Link href="/issues/new">New Issue</Link></Button>;
 };
 
 export default IssuesPage;
