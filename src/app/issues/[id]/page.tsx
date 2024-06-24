284c64e1b92769512399c75c3e5882a7b97bb732
Mahesh CHODA <choda.maheshbabu@gmail.com>
2024-06-24 18:53:37 +0530

    Refactor: Apply the Single Responsibility Principle.

--- src/app/issues/[id]/page.tsx
+++ src/app/issues/[id]/page.tsx
@@ -1,41 +1,32 @@
-import { IssueStatusBadge } from '@/app/components';
 import prisma from '@/app/lib/client';
-import { Pencil2Icon } from '@radix-ui/react-icons';
-import { Box, Button, Card, Flex, Grid, Heading, Text } from '@radix-ui/themes';
-import Link from 'next/link';
+import { Box, Grid } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
-import Markdown from 'react-markdown';
+import EditIssueButton from './EditIssueButton';
+import IssueDetails from './IssueDetails';
 
 interface Props {
   params: { id: string };
 }
 
-const IssueDetailPage = async ({ params: { id } }: Props) => {
-  const issue = await prisma.issue.findUnique({
+const getIssue = async (id: string) => {
+  return await prisma.issue.findUnique({
     where: {
       id: parseInt(id),
     },
   });
+};
 
+const IssueDetailPage = async ({ params: { id } }: Props) => {
+  const issue = await getIssue(id);
   if (!issue) notFound();
 
   return (
     <Grid columns={{ initial: '1', md: '2' }} gap="5">
       <Box>
-        <Heading>{issue.title}</Heading>
-        <Flex gap="4" my="2" align="center">
-          <IssueStatusBadge status={issue.status} />
-          <Text>{issue.createdAt.toDateString()}</Text>
-        </Flex>
-        <Card className="prose" mt="4">
-          <Markdown>{issue.description}</Markdown>
-        </Card>
+        <IssueDetails issue={issue} />
       </Box>
       <Box>
-        <Button>
-          <Pencil2Icon />
-          <Link href={`/issues/${issue.id}/edit`}> Edit Issue </Link>
-        </Button>
+        <EditIssueButton issueId={issue.id} />
       </Box>
     </Grid>
   );
