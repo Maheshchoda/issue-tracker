044b77dd0621774fa8ad37aafb65c6428137133c
Mahesh CHODA <choda.maheshbabu@gmail.com>
2024-06-26 15:07:27 +0530

    Add Delete Issue API

--- src/app/api/issues/[id]/route.tsx
+++ src/app/api/issues/[id]/route.tsx
@@ -7,16 +7,20 @@ interface Props {
   params: { id: string };
 }
 
+const getIssue = async (issueId: string) => {
+  return await prisma.issue.findUnique({
+    where: {
+      id: parseInt(issueId),
+    },
+  });
+};
+
 export async function PATCH(request: NextRequest, { params: { id: issueId } }: Props) {
   const body: IssueSchemaType = await request.json();
   const validation = issueSchema.safeParse(body);
   if (!validation.success) return NextResponse.json(validation.error.format(), { status: 400 });
 
-  const issue = await prisma.issue.findUnique({
-    where: {
-      id: parseInt(issueId),
-    },
-  });
+  const issue = await getIssue(issueId);
 
   if (!issue) return NextResponse.json({ error: 'Invalid Issue' }, { status: 404 });
 
@@ -31,4 +35,21 @@ export async function PATCH(request: NextRequest, { params: { id: issueId } }: P
   });
 
   return NextResponse.json(updatedIssue);
+}
+
+export async function DELETE(request: NextRequest, { params: { id: issueId } }: Props) {
+  const issue = await getIssue(issueId);
+
+  if (!issue) return NextResponse.json({ error: 'Invalid Issue' }, { status: 404 });
+
+  try {
+    await prisma.issue.delete({
+      where: {
+        id: issue.id,
+      },
+    });
+    return NextResponse.json({ });
+  } catch (e) {
+    return NextResponse.json({ error: 'Un expected error occured' });
+  }
 }
