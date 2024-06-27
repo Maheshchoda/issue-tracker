f008fcd83b03d7f3141a0b8699458c971015eb8f
Mahesh CHODA <choda.maheshbabu@gmail.com>
2024-06-27 19:59:49 +0530

    Protect New Issue creation.

--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -1,12 +1,16 @@
 import dynamic from 'next/dynamic';
 import IssueFormSkeleton from './loading';
+import { auth } from '@/app/auth';
+import { redirect } from 'next/navigation';
 
 const IssueForm = dynamic(() => import('@/app/issues/_components/IssueForm'), {
   ssr: false,
   loading: () => <IssueFormSkeleton />,
 });
 
-const NewIssuePage = () => {
+const NewIssuePage = async () => {
+  const session = await auth();
+  if (!session) redirect('/api/auth/signin');
   return <IssueForm />;
 };
 
