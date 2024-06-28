a06b9c1a73f33deb05eaddf6e7eaf2af5c80e4f8
Mahesh CHODA <choda.maheshbabu@gmail.com>
2024-06-28 18:28:00 +0530

    Make Columns sortable.

--- src/app/issues/list/page.tsx
+++ src/app/issues/list/page.tsx
@@ -2,13 +2,20 @@ import { IssueStatusBadge, Link } from '@/app/components';
 import { Table } from '@radix-ui/themes';
 import prisma from '../../lib/client';
 import IssueToolbar from './IssueToolbar';
-import { Status } from '@prisma/client';
+import NextLink from 'next/link';
+import { Issue, Status } from '@prisma/client';
+import { ArrowUpIcon } from '@radix-ui/react-icons';
 
 interface Props {
-  searchParams: { status: Status };
+  searchParams: { status: Status; orderBy: keyof Issue };
 }
 
 const IssuesPage = async ({ searchParams }: Props) => {
+  const sortBy: { label: string; value: keyof Issue; className?: string }[] = [
+    { label: 'Issue', value: 'title' },
+    { label: 'Status', value: 'status', className: 'hidden md:table-cell' },
+    { label: 'Created', value: 'createdAt', className: 'hidden md:table-cell' },
+  ];
   const statuses = Object.values(Status);
   const status = statuses.includes(searchParams.status) ? searchParams.status : undefined;
   const issues = await prisma.issue.findMany({
@@ -23,11 +30,14 @@ const IssuesPage = async ({ searchParams }: Props) => {
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
-            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">Status</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Created
-            </Table.ColumnHeaderCell>
+            {sortBy.map((sortItem) => (
+              <Table.ColumnHeaderCell key={sortItem.value} className={sortItem.className}>
+                <NextLink href={{ query: { ...searchParams, orderBy: sortItem.value } }}>
+                  {sortItem.label}
+                  {sortItem.value === searchParams.orderBy && <ArrowUpIcon className="inline" />}
+                </NextLink>
+              </Table.ColumnHeaderCell>
+            ))}
           </Table.Row>
         </Table.Header>
         <Table.Body>
