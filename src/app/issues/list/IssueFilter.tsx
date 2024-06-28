e918e798be7e351b4ce4423f4e7ffea1068b3696
Mahesh CHODA <choda.maheshbabu@gmail.com>
2024-06-28 17:42:29 +0530

    Filter Issues.

--- src/app/issues/list/IssueFilter.tsx
+++ src/app/issues/list/IssueFilter.tsx
@@ -1,26 +1,28 @@
 'use client';
 import { Status } from '@prisma/client';
 import { Select } from '@radix-ui/themes';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
-const statuses: { label: string; value: Status | 'all_statuses' }[] = [
-  { label: 'All', value: 'all_statuses' },
+const statuses: { label: string; value?: Status }[] = [
+  { label: 'All' },
   { label: 'Open', value: 'OPEN' },
   { label: 'In Progress', value: 'IN_PROGRESS' },
   { label: 'Closed', value: 'CLOSED' },
 ];
 
 const IssueFilter = () => {
-  const handleStatusChange = (value: string) => {
-    const selectedStatus = value === 'all_statuses' ? undefined : (value as Status);
-    console.log('selected Status', selectedStatus);
+  const router = useRouter();
+  const handleStatusChange = (status: string) => {
+    const query = status !== 'all_statuses' ? `?status=${status}` : '';
+    router.push('/issues/list' + query);
   };
   return (
     <Select.Root onValueChange={handleStatusChange}>
       <Select.Trigger placeholder="Select by Status...." />
       <Select.Content>
-        {statuses.map((status) => (
-          <Select.Item key={status.value} value={status.value}>
+        {statuses.map((status, index) => (
+          <Select.Item key={index} value={status.value || 'all_statuses'}>
             {status.label}
           </Select.Item>
         ))}
