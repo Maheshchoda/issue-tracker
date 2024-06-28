b7a5f4d579979fccb94f00dbfa3a26b757726221
Mahesh CHODA <choda.maheshbabu@gmail.com>
2024-06-28 12:18:20 +0530

    Build Assignee select component.

--- src/app/issues/[id]/page.tsx
+++ src/app/issues/[id]/page.tsx
@@ -5,6 +5,7 @@ import { notFound } from 'next/navigation';
 import DeleteIssueButton from './DeleteIssueButton';
 import EditIssueButton from './EditIssueButton';
 import IssueDetails from './IssueDetails';
+import AsigneeSelect from './AsigneeSelect';
 
 interface Props {
   params: { id: string };
@@ -32,6 +33,7 @@ const IssueDetailPage = async ({ params: { id } }: Props) => {
       {session && (
         <Box className="col-span-1">
           <Flex direction="column" gap="4">
+            <AsigneeSelect />
             <EditIssueButton issueId={issue.id} />
             <DeleteIssueButton issueId={issue.id} />
           </Flex>
