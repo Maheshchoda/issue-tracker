6590ac45f777592f57ba45684ccb783a5420bc7a
Mahesh CHODA <choda.maheshbabu@gmail.com>
2024-06-29 14:37:41 +0530

    Refactor Extract Issue Table.

--- src/app/issues/list/page.tsx
+++ src/app/issues/list/page.tsx
@@ -1,86 +1,43 @@
-import { IssueStatusBadge, Link } from '@/app/components';
-import { Table } from '@radix-ui/themes';
+import Pagniation from '@/app/components/Pagniation';
+import { Status } from '@prisma/client';
 import prisma from '../../lib/client';
+import IssueTable, { IssueQuery, sortNames } from './IssueTable';
 import IssueToolbar from './IssueToolbar';
-import NextLink from 'next/link';
-import { Issue, Status } from '@prisma/client';
-import { ArrowDownIcon, ArrowUpIcon } from '@radix-ui/react-icons';
+import { Flex } from '@radix-ui/themes';
 
 interface Props {
-  searchParams: { status: Status; orderBy: keyof Issue; order: 'asc' | 'desc' };
+  searchParams: IssueQuery;
 }
 
 const IssuesPage = async ({ searchParams }: Props) => {
-  const sortBy: { label: string; value: keyof Issue; className?: string }[] = [
-    { label: 'Issue', value: 'title' },
-    { label: 'Status', value: 'status', className: 'hidden md:table-cell' },
-    { label: 'Created', value: 'createdAt', className: 'hidden md:table-cell' },
-  ];
   const statuses = Object.values(Status);
   const status = statuses.includes(searchParams.status) ? searchParams.status : undefined;
   const order =
     searchParams.order === 'asc' || searchParams.order === 'desc' ? searchParams.order : 'asc';
-  const orderBy = sortBy.map((item) => item.value).includes(searchParams.orderBy)
+  const orderBy = sortNames.includes(searchParams.orderBy)
     ? { [searchParams.orderBy]: order }
     : undefined;
+
+  const where = { status: status };
+
+  const pageSize = 10;
+  const currentPage = parseInt(searchParams.page) || 1;
+
   const issues = await prisma.issue.findMany({
-    where: {
-      status,
-    },
+    where,
     orderBy,
+    skip: (currentPage - 1) * pageSize,
+    take: pageSize,
   });
 
-  const getNextOrder = (currentOrder: 'asc' | 'desc') => (currentOrder === 'asc' ? 'desc' : 'asc');
+  const issueCount = await prisma.issue.count({ where });
 
   return (
-    <>
+    <Flex direction="column" gap="5">
       <IssueToolbar />
-      <Table.Root variant="surface">
-        <Table.Header>
-          <Table.Row>
-            {sortBy.map((sortItem) => (
-              <Table.ColumnHeaderCell key={sortItem.value} className={sortItem.className}>
-                <NextLink
-                  href={{
-                    query: {
-                      ...searchParams,
-                      orderBy: sortItem.value,
-                      order: getNextOrder(searchParams.order),
-                    },
-                  }}
-                >
-                  {sortItem.label}
-                  {sortItem.value === searchParams.orderBy &&
-                    (searchParams.order === 'asc' ? (
-                      <ArrowUpIcon className="inline" />
-                    ) : (
-                      <ArrowDownIcon className="inline" />
-                    ))}
-                </NextLink>
-              </Table.ColumnHeaderCell>
-            ))}
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {issues.map((issue) => (
-            <Table.Row key={issue.id}>
-              <Table.Cell>
-                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
-                <div className="block md:hidden">
-                  <IssueStatusBadge status={issue.status} />
-                </div>
-              </Table.Cell>
-              <Table.Cell className="hidden md:table-cell">
-                <IssueStatusBadge status={issue.status} />
-              </Table.Cell>
-              <Table.Cell className="hidden md:table-cell">
-                {issue.createdAt.toDateString()}
-              </Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table.Root>
-    </>
+      <IssueTable searchParams={searchParams} issues={issues} />
+      <Pagniation itemCount={issueCount} pageSize={pageSize} currentPage={currentPage} />
+    </Flex>
   );
 };
 
