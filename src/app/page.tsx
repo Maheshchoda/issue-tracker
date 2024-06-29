ed60df258a1b1487a9ec1db6583688b108f0e4bf
Mahesh CHODA <choda.maheshbabu@gmail.com>
2024-06-29 20:05:26 +0530

    Fetch Count Moved to Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
+import { Flex } from '@radix-ui/themes';
+import IssueSummary from './IssueSummary';
 import LatestIssues from './LatestIssues';
+import { Status } from '@prisma/client';
+import prisma from './lib/client';
 
-export default function Home() {
+export type StatusCountType = {
+  [key in Status]: number;
+};
+
+export default async function Home() {
+  const getCount = async (status: Status): Promise<number> => {
+    return await prisma.issue.count({ where: { status } });
+  };
+  const statusCount: StatusCountType = {
+    OPEN: await getCount(Status.OPEN),
+    IN_PROGRESS: await getCount(Status.IN_PROGRESS),
+    CLOSED: await getCount(Status.CLOSED),
+  };
   return (
-    <>
+    <Flex  justify="between" align="start">
+      <IssueSummary statusCount={statusCount} />
       <LatestIssues />
-    </>
+    </Flex>
   );
 }
